3ecb4edbb6e20b5f1f7f6c3bf0b93717c8a7f454
soheyHashimoto <heyso1209@gmail.com>
2022-02-18 10:38:17 +0900

    [ADD]StatefulWidgetとしてRandomWordsを追加

--- lib/main.dart
+++ lib/main.dart
@@ -74,3 +74,17 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
